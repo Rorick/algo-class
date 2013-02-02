55bf919f39d13207470e9440178524e520c730d7
Rorick <rorick@gmail.com>
2013-02-02 22:00:11 +0400

    Introduce ShouldMatchers.

--- src/test/scala/org/rorick/algoclass/week1/merge/mergesortTest.scala
+++ src/test/scala/org/rorick/algoclass/week1/merge/mergesortTest.scala
@@ -1,67 +1,68 @@
 package org.rorick.algoclass.week1.merge
 
 import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
 
 /**
  * Test case for [[org.rorick.algoclass.week1.merge.mergesort]].
  */
-class mergesortTest extends FunSuite {
+class mergesortTest extends FunSuite with ShouldMatchers {
   import mergesort._
 
   test("should return same list when zero element list") {
-    assert(sort(List()) === List())
+    sort(List()) should equal (List())
   }
 
   test("should return same list when one element list") {
-    assert(sort(List(2)) === List(2))
+    sort(List(2)) should equal (List(2))
   }
 
   test("should return same list when two element sorted list") {
-    assert(sort(List(2, 5)) === List(2, 5))
+    sort(List(2, 5)) should equal (List(2, 5))
   }
 
   test("should return same list when three element sorted list") {
-    assert(sort(List(2, 5, 6)) === List(2, 5, 6))
+    sort(List(2, 5, 6)) should equal (List(2, 5, 6))
   }
 
   test("should return sorted list when two element unsorted list") {
-    assert(sort(List(5, 2)) === List(2, 5))
+    sort(List(5, 2)) should equal (List(2, 5))
   }
 
   test("should return sorted list when three element unsorted list") {
-    assert(sort(List(5, 2, 4)) === List(2, 4, 5))
-    assert(sort(List(5, 4, 2)) === List(2, 4, 5))
-    assert(sort(List(4, 2, 5)) === List(2, 4, 5))
-    assert(sort(List(4, 5, 2)) === List(2, 4, 5))
-    assert(sort(List(2, 5, 4)) === List(2, 4, 5))
-    assert(sort(List(2, 4, 5)) === List(2, 4, 5))
+    sort(List(5, 2, 4)) should equal (List(2, 4, 5))
+    sort(List(5, 4, 2)) should equal (List(2, 4, 5))
+    sort(List(4, 2, 5)) should equal (List(2, 4, 5))
+    sort(List(4, 5, 2)) should equal (List(2, 4, 5))
+    sort(List(2, 5, 4)) should equal (List(2, 4, 5))
+    sort(List(2, 4, 5)) should equal (List(2, 4, 5))
   }
 
   test("should return sorted list when four element list") {
-    assert(sort(List(1, 2, 3, 4)) === List(1, 2, 3, 4))
-    assert(sort(List(1, 2, 4, 3)) === List(1, 2, 3, 4))
-    assert(sort(List(1, 3, 2, 4)) === List(1, 2, 3, 4))
-    assert(sort(List(1, 3, 4, 2)) === List(1, 2, 3, 4))
-    assert(sort(List(1, 4, 2, 3)) === List(1, 2, 3, 4))
-    assert(sort(List(1, 4, 3, 2)) === List(1, 2, 3, 4))
-    assert(sort(List(2, 1, 3, 4)) === List(1, 2, 3, 4))
-    assert(sort(List(2, 1, 4, 3)) === List(1, 2, 3, 4))
-    assert(sort(List(2, 3, 1, 4)) === List(1, 2, 3, 4))
-    assert(sort(List(2, 3, 4, 1)) === List(1, 2, 3, 4))
-    assert(sort(List(2, 4, 1, 3)) === List(1, 2, 3, 4))
-    assert(sort(List(2, 4, 3, 1)) === List(1, 2, 3, 4))
-    assert(sort(List(3, 1, 2, 4)) === List(1, 2, 3, 4))
-    assert(sort(List(3, 1, 4, 2)) === List(1, 2, 3, 4))
-    assert(sort(List(3, 2, 1, 4)) === List(1, 2, 3, 4))
-    assert(sort(List(3, 2, 4, 1)) === List(1, 2, 3, 4))
-    assert(sort(List(3, 4, 1, 2)) === List(1, 2, 3, 4))
-    assert(sort(List(3, 4, 2, 1)) === List(1, 2, 3, 4))
-    assert(sort(List(4, 1, 2, 3)) === List(1, 2, 3, 4))
-    assert(sort(List(4, 1, 3, 2)) === List(1, 2, 3, 4))
-    assert(sort(List(4, 2, 1, 3)) === List(1, 2, 3, 4))
-    assert(sort(List(4, 2, 3, 1)) === List(1, 2, 3, 4))
-    assert(sort(List(4, 3, 1, 2)) === List(1, 2, 3, 4))
-    assert(sort(List(4, 3, 2, 1)) === List(1, 2, 3, 4))
+    sort(List(1, 2, 3, 4)) should equal (List(1, 2, 3, 4))
+    sort(List(1, 2, 4, 3)) should equal (List(1, 2, 3, 4))
+    sort(List(1, 3, 2, 4)) should equal (List(1, 2, 3, 4))
+    sort(List(1, 3, 4, 2)) should equal (List(1, 2, 3, 4))
+    sort(List(1, 4, 2, 3)) should equal (List(1, 2, 3, 4))
+    sort(List(1, 4, 3, 2)) should equal (List(1, 2, 3, 4))
+    sort(List(2, 1, 3, 4)) should equal (List(1, 2, 3, 4))
+    sort(List(2, 1, 4, 3)) should equal (List(1, 2, 3, 4))
+    sort(List(2, 3, 1, 4)) should equal (List(1, 2, 3, 4))
+    sort(List(2, 3, 4, 1)) should equal (List(1, 2, 3, 4))
+    sort(List(2, 4, 1, 3)) should equal (List(1, 2, 3, 4))
+    sort(List(2, 4, 3, 1)) should equal (List(1, 2, 3, 4))
+    sort(List(3, 1, 2, 4)) should equal (List(1, 2, 3, 4))
+    sort(List(3, 1, 4, 2)) should equal (List(1, 2, 3, 4))
+    sort(List(3, 2, 1, 4)) should equal (List(1, 2, 3, 4))
+    sort(List(3, 2, 4, 1)) should equal (List(1, 2, 3, 4))
+    sort(List(3, 4, 1, 2)) should equal (List(1, 2, 3, 4))
+    sort(List(3, 4, 2, 1)) should equal (List(1, 2, 3, 4))
+    sort(List(4, 1, 2, 3)) should equal (List(1, 2, 3, 4))
+    sort(List(4, 1, 3, 2)) should equal (List(1, 2, 3, 4))
+    sort(List(4, 2, 1, 3)) should equal (List(1, 2, 3, 4))
+    sort(List(4, 2, 3, 1)) should equal (List(1, 2, 3, 4))
+    sort(List(4, 3, 1, 2)) should equal (List(1, 2, 3, 4))
+    sort(List(4, 3, 2, 1)) should equal (List(1, 2, 3, 4))
   }
 
   test("should sort random 5 element lists") {
@@ -87,14 +88,17 @@ class mergesortTest extends FunSuite {
   }
 
   test("should sort 5 element list") {
-    assert(sort(List(3, 4, 5, 1, 2)) === List(1, 2, 3, 4, 5))
+    sort(List(3, 4, 5, 1, 2)) should equal (List(1, 2, 3, 4, 5))
   }
 
   test("should return new list") {
     val unsortedList = List(5, 2)
-    assert(!(sort(unsortedList) eq unsortedList))
+    sort(unsortedList) should (not be theSameInstanceAs (unsortedList))
 
     val sortedList = List(3, 4)
-    assert(!(sort(sortedList) eq sortedList))
+    sort(sortedList) should (not be theSameInstanceAs (sortedList))
+
+    val emptyList = List()
+    sort(emptyList) should (be theSameInstanceAs (emptyList))
   }
 }
