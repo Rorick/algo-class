e81d0e248c271e5ef95e871f217bac8c590b6799
Rorick <rorick@gmail.com>
2013-02-21 23:14:26 +0400

    Add possibility to pass custom node selector into MinCutFinder. This will allow to use random node selection.

--- src/main/scala/org/rorick/algoclass/week3/MinCutFinder.scala
+++ src/main/scala/org/rorick/algoclass/week3/MinCutFinder.scala
@@ -4,41 +4,18 @@ package org.rorick.algoclass.week3
 /**
  * Implementation of randomized min cut search algorithm.
  */
-class MinCutFinder(incidents: List[List[Int]]) {
+class MinCutFinder(incidents: List[List[Int]], nextNode: (Int => Int)) {
+  def this(incidents: List[List[Int]]) = this(incidents, _ => 0)
 
   lazy val minCutSize = calculateMinCutSize(new Graph(incidents))
 
 
   private def calculateMinCutSize(graph: Graph): Int = {
 
-    //    if (incidents.size > 1) 1 else 0
     // divide by two because graph is undirected and has edges in both directions
     if (graph.nodes.size > 2) {
       // pick random edge
-      graph.contractNode(0)
-      /*val edge = graph.edges(0)
-      // contract it
-      // update nodes
-      graph.nodes -= edge._1
-      graph.nodes -= edge._2
-      val superNode: Node = new MergedNode(edge._1, edge._2)
-      graph.nodes += superNode
-      // update edges
-      graph.edges -= ((edge._1, edge._2))
-      graph.edges -= ((edge._2, edge._1))
-      graph.edges ++= graph.incidentals(edge._1).filter(_ !=(edge._1, edge._2)).map{case (n1, n2) => (superNode, n2)}
-      graph.edges ++= graph.incidentals(edge._2).filter(_ !=(edge._2, edge._1)).map{case (n1, n2) => (superNode, n2)}
-      // update incidents
-      val newIncidentals = (
-        graph.incidentals(edge._1).filter(_ !=(edge._1, edge._2))
-          union
-          graph.incidentals(edge._2).filter(_ !=(edge._2, edge._1)))
-
-      graph.incidentals.remove(edge._1)
-      graph.incidentals.remove(edge._2)
-      graph.incidentals(superNode) = newIncidentals
-
-      // remove self loops*/
+      graph.contractNode(nextNode(graph.edges.size))
       // call recursively
       calculateMinCutSize(graph)
     } else {
