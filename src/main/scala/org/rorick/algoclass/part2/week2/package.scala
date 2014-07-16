610119b040c98bd8436bccd5d15cb9268cd4eae0
Roman Belous <rbelous@griddynamics.com>
2014-07-17 01:47:55 +0400

    Add package with doc for week2.

---
+++ src/main/scala/org/rorick/algoclass/part2/week2/package.scala
@@ -0,0 +1,29 @@
+package org.rorick.algoclass.part2
+
+/**
+ * Programming assignment for week2. It covers the following.
+ *
+ * KRUSKAL'S MST ALGORITHM: Last week we covered Prim's MST algorithm and a blazingly fast implementation of it. There
+ * are several reasons for studying a second greedy MST algorithm, due to Kruskal. First, it's a beautiful algorithm,
+ * worthy of a greatest hits compilation. Second, to obtain a super-fast implementation of it, we'll need to learn about
+ * the simple but fundamental "Union-Find" data structure. The third reason is covered in the next section...
+ *
+ * CLUSTERING: Clustering is an important form of unsupervised learning (i.e., extracting patterns from unlabeled data).
+ * These two videos discuss how Kruskal's MST algorithm suggests flexible and useful greedy approaches to clustering
+ * problems.
+ *
+ * HUFFMAN CODES: Everybody loves compression. It means more songs on your smartphone, faster downloads, and so on.
+ * Huffman coding is a fundamental type of lossless compression, used for example in the MP3 standard. In these videos
+ * we'll learn about the optimality of Huffman codes, and a blazingly fast greedy algorithm for computing them.
+ *
+ * UNION-FIND LECTURES: This is purely optional material about advanced implementations of the union-find data
+ * structure. For those of you looking for some seriously next-level (but beautiful) material, check them out when you
+ * get a chance.
+ *
+ * The second programming assignment, also due July 20th (with 10 attempts), asks you to implement the greedy clustering
+ * algorithm from lecture. For part (a), a straightforward implementation should suffice. Part (b) involves a graph that
+ * is likely too big to fit in your computer's memory, so answering this part will take some ingenuity.
+ */
+package object week2 {
+
+}
