4865621b976d5af11aafe48f7eb4dde498233cbe
Roman Belous <rbelous@griddynamics.com>
2014-07-23 12:20:36 +0400

    Make an algo skeleton.

--- src/main/scala/org/rorick/algoclass/part2/week2/BigMaxSpacingKClustering.scala
+++ src/main/scala/org/rorick/algoclass/part2/week2/BigMaxSpacingKClustering.scala
@@ -35,20 +35,70 @@ import scala.collection.mutable
 object BigMaxSpacingKClustering extends App {
   val lines = io.Source.fromInputStream(getClass.getResourceAsStream("clustering_big.txt")).getLines()
   val Array(n, numBits) = lines next() split " " map (_.toInt)
-  val points = mutable.Set[Point]
+  val points = mutable.Set.empty[Point]
   lines foreach { line =>
-    points += Integer parseInt (line replace (" ", ""), 2)
+    points += Integer.parseInt(line replace(" ", ""), 2)
     // Note: point value is not unique
     // so we need to track each point with number.. Or not? Because distance is determined by value, hence value is a
     // kind of coordinates so equal points have the same coordinates and all distances to all other points will be the
     // same. But it affects number of clusters. Still, forums offer to use sets to ignore
   }
 
-  // take point
-  // generate points differing in l bits
-  // remove points not in set
-  // generate only lexicographically larger to avoid reverse edges
-  // somehow need to track processed edges (queue?)
+  val X = UnionFind(points.toSeq: _*)
+  val sortedDistances = mutable.Queue.empty[D]
+  var currentMinimalDistance = 1
+  do {
+    while (sortedDistances.isEmpty) {
+      points foreach { p =>
+        val qs = bitFlipPositions(currentMinimalDistance) map toMask map (flipBits(p, _))
+        sortedDistances ++= (for (q <- qs if X.find(q)) yield D(p, q, currentMinimalDistance))
+      }
+      currentMinimalDistance = currentMinimalDistance + 1
+    }
+    val d = sortedDistances.dequeue()
+    if (areSeparated(d.p, d.q)) {
+      X.union(d.p, d.q)
+    }
+  } while (spacing < 3)
+  println(X.size)
 
-//  Integer.
+  private def areSeparated(p: Point, q: Point): Boolean = X.find(p) != X.find(q)
+
+  def spacing: Int = (sortedDistances filter (d => areSeparated(d.p, d.q))).head.distance
+
+  def bitFlipPositions(n: Int): List[List[Int]] = {
+    if (n == 0)
+      List(List.empty)
+    else for {
+      position <- 0 until 24
+      restBitPositions <- bitFlipPositions(n - 1)
+      if restBitPositions.isEmpty || position < restBitPositions.head
+    } yield position :: restBitPositions
+  }.toList
+
+  /**
+   * Converts a number of bit positions starting from zero to mask with ones in specified bit positions.
+   *
+   * @param bitPositions list of bit positions to be flipped
+   * @return binary mask with ones set in specified positions
+   */
+  def toMask(bitPositions: List[Int]): Int = {
+    bitPositions map (1 << _) reduce (_ | _)
+  }
+
+//  bitFlipPositions(2) map toMask map (flipBits(v, _)) map toBitString
+
+  def fromBitString(s: String): Int = Integer.parseInt(s, 2)
+
+  def toBitString(i: Int): String = "%24s".format(Integer.toBinaryString(i)).replace(' ', '0')
+
+  /**
+   * Flip bits in positions specified by mask
+   *
+   * @param v value whose bits are to be flipped
+   * @param mask mask specifying bits to be flipped
+   * @return value with flipped bits
+   */
+  def flipBits(v: Int, mask: Int): Int = v ^ mask
+
 }
